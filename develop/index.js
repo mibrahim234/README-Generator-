fbb4a62c359f298d6971615ecf5a54a13d13a807
mibrahim234 <mohamm1417@gmail.com>
2021-10-06 03:14:34 -0400

    boom

--- develop/index.js
+++ develop/index.js
@@ -2,6 +2,17 @@
 
 // TODO: Create an array of questions for user input
 const questions = [];
+// What is your GitHub username?
+// What is your email address?
+// What is your project's name?
+// Please write a short description of your project
+// What kind of license should your project have? (use arrow keys)
+// -- Mit, Apache 2.0, GPL 3.0, BSD 3, None
+// What command should be run to install dependencies? (npm i)
+// What command should be run to run tests? (npm test)
+// What does the user need to know about using the repo?
+// What does the user need to know about contributing to the repo?
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
