07905e5768c6318d74dbea40372669327bd65b4f
mibrahim234 <mohamm1417@gmail.com>
2021-10-07 00:42:45 -0400

    added questions

--- develop/index.js
+++ develop/index.js
@@ -1,17 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeReadMe = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
-// What is your GitHub username?
-// What is your email address?
-// What is your project's name?
-// Please write a short description of your project
-// What kind of license should your project have? (use arrow keys)
-// -- Mit, Apache 2.0, GPL 3.0, BSD 3, None
-// What command should be run to install dependencies? (npm i)
-// What command should be run to run tests? (npm test)
-// What does the user need to know about using the repo?
-// What does the user need to know about contributing to the repo?
+const questions = [
+    {
+      type: "input",
+      message: "What is the title of the project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What is your project site URL?",
+      name: "url",
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter in installation instructions of your project:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Enter in the usage information for your project:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Would you like to credit anyone for this project?",
+      name: "credits",
+    },
+    {
+      type: "input",
+      message: "Enter in the test instructions for this project:",
+      name: "tests",
+    },
+    {
+      type: "list",
+      message: "Choose a license from the following list:",
+      choices: [
+        "Apache License 2.0",
+        "GNU General Public License v3.0",
+        "MIT License",
+        // "BSD T-Clause 'Simplified' License",
+        // "BSD 3-Clause 'New' or 'Revised' License",
+        // "Creative Commons Zero v1.0 Universal",
+        // "Eclipse Public License 2.0",
+        // "GNU Affero General Public License v2.1",
+        // "GNU General Public License v2.0",
+        // "GNU Lesser General Public License v2.1",
+        // "GNU Lesser General Public License v3.0",
+        "Mozilla Public License 2.0",
+        "The Unilicense",
+      ],
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ];
 
 
 // TODO: Create a function to write README file
